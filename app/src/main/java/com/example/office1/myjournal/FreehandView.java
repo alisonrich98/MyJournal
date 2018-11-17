3ff81eaa556281f4b6d85ec01d4387aedf96da98
Claire <redreadergirl@hotmail.com>
2018-11-16 21:33:53 -0700

    create freehand view

---
+++ app/src/main/java/com/example/office1/myjournal/FreehandView.java
@@ -0,0 +1,16 @@
+package com.example.office1.myjournal;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class FreehandView extends View {
+    public FreehandView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setupPage();
+    }
+
+    private void setupPage() {
+
+    }
+}
