d0dc197cfea50d4f8b770401023e27be34db5509
Claire <redreadergirl@hotmail.com>
2018-11-17 10:47:38 -0700

    add prompts

--- app/src/main/java/com/example/office1/myjournal/JournalEntry.java
+++ app/src/main/java/com/example/office1/myjournal/JournalEntry.java
@@ -1,11 +1,14 @@
 package com.example.office1.myjournal;
 
+import android.media.Image;
+
 import java.io.File;
 import java.time.LocalDateTime;
 
 public class JournalEntry {
 
     String text;
+    Image image;
     LocalDateTime dateTime;
     File filename;
 }
--- app/src/main/java/com/example/office1/myjournal/PromptEntry.java
+++ app/src/main/java/com/example/office1/myjournal/PromptEntry.java
@@ -6,19 +6,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class PromptEntry extends AppCompatActivity {
     int textColor;
     private FreehandView drawView;
+    Button randomize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prompt_entry);
+        randomize = (Button)findViewById(R.id.button5);
 
         drawView = (FreehandView)findViewById(R.id.freehand);
         Spinner spinner = (Spinner) findViewById(R.id.spinner2);
@@ -54,5 +58,18 @@ public class PromptEntry extends AppCompatActivity {
 
             }
         });
+
+        randomize.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                randomize.setText(getRandomPrompt());
+            }
+        });
+    }
+
+    public String getRandomPrompt() {
+        String[] prompts = getResources().getStringArray(R.array.prompts);
+        int randomNumber = new Random().nextInt(prompts.length);
+        return prompts[randomNumber];
     }
 }
