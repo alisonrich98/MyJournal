d1537196f74bcdb52fbd2ce8ac9f944d6556c79b
Alison Rich <Office1@SOO1-iMac.local>
2018-11-17 08:32:47 -0700

    part6

---
+++ app/src/main/java/com/example/office1/myjournal/NoPromptEntry.java
@@ -0,0 +1,13 @@
+package com.example.office1.myjournal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoPromptEntry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_prompt_entry);
+    }
+}
